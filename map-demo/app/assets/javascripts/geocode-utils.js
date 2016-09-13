91296a1b4ccdb8d272c56d9d44660a1c58f313e9
Adam Fallon <afallon0@icloud.com>
2016-09-13 09:48:49 +0100

    + Returns Data from Mapbox API given a search parameter

---
+++ map-demo/app/assets/javascripts/geocode-utils.js
@@ -0,0 +1,26 @@
+/*
+    Created by Adam Fallon
+    Email: a.fallon@kainos.com
+
+    Geocode-utils : Takes input of Postcode and outputs location data from Mapbox
+
+    Need to include these files before adding geocode-utils.js
+        <script src ='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+        <script src='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js'></script>
+
+  This is optional styling if you are directly using MapBox as the rendering engine
+        <link href='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.css' rel='stylesheet' /> 
+*/
+L.mapbox.accessToken = 'ADD_ACCESS_TOKEN_HERE'
+var geocoder = L.mapbox.geocoder('mapbox.places')
+
+function geocode (searchQuery) {
+  geocoder.query(searchQuery, function (err, data) {
+    if (!err) {
+      return data
+    } else {
+      console.log(err)
+      return err
+    }
+  })
+}
