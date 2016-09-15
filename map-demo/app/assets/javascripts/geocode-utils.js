8b104f9728328089e4045df83964793ed09ab147
Adam Fallon <afallon0@icloud.com>
2016-09-15 16:14:43 +0100

    Updated comments

--- map-demo/app/assets/javascripts/geocode-utils.js
+++ map-demo/app/assets/javascripts/geocode-utils.js
@@ -5,11 +5,7 @@
     Geocode-utils : Takes input of Postcode and outputs location data from Mapbox
 
     Need to include these files before adding geocode-utils.js
-        <script src ='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
         <script src='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js'></script>
-
-  This is optional styling if you are directly using MapBox as the rendering engine
-        <link href='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.css' rel='stylesheet' /> 
 */
 
 var config = new Config()
