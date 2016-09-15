023d2033ebf9349a3c6344430f456323b0512339
Adam Fallon <a.fallon@kainos.com>
2016-09-15 19:24:35 +0100

    Feature/geocoding (#11)
    * + Returns Data from Mapbox API given a search parameter
    
    * Added Searchbox
    + Users can now search by postcode of location
    
    * Added checkboxes for layer toggle
    
    * Fixed the form block labels and validation not included in CSS
    Made the radio boxes inline
    
    * Fixed lables clicking label not selecting checkbox
    
    * Added Fallback Postcode Lookup
    
    * Pulled out API key to config file.
    
    * Code Review Changes
    
    * Updated comments

---
+++ map-demo/app/assets/javascripts/geocode-utils.js
@@ -0,0 +1,48 @@
+/*
+    Created by Adam Fallon
+    Email: a.fallon@kainos.com
+
+    Geocode-utils : Takes input of Postcode and outputs location data from Mapbox
+
+    Need to include these files before adding geocode-utils.js
+        <script src='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js'></script>
+*/
+
+var config = new Config()
+L.mapbox.accessToken = config.mapboxKey()
+
+var geocoder = L.mapbox.geocoder('mapbox.places')
+const ZOOM_LEVEL = 17
+
+function geocode (searchQuery) {
+  geocoder.query(searchQuery, function (err, data) {
+    if (!err) {
+      if (data.hasOwnProperty('latlng')) {
+        $('#error').slideUp("slow")
+        var utils = new MapUtils()
+        utils.moveViewPort(data.latlng[0], data.latlng[1], ZOOM_LEVEL)
+        return data.latlng
+      } else {
+        postCodeBackup(searchQuery)
+      }
+    } else {
+      postCodeBackup(searchQuery)
+    }
+  })
+}
+function postCodeBackup (searchQuery) {
+  $.ajax({
+    url: 'https://api.postcodes.io/postcodes/' + searchQuery,
+    success: function (data) {
+      $('#error').slideUp("slow")
+      var utils = new MapUtils()
+      utils.moveViewPort(data.result.latitude, data.result.longitude, ZOOM_LEVEL)
+    },
+    error: function (err) {
+      console.log('Postcode Not Found')
+      $('#postcode-error').text("We could not find " + searchQuery)
+      $('#error').slideDown("slow")
+    }
+  })
+}
+
