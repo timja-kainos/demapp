9b77c294d056cb5c9cb56fcb1cb8aea953113017
Ashleigh Hood <a.hood@kainos.com>
2016-09-15 12:21:32 +0100

    Added toggle for changing base map Moved toggle outside of leaflet map

--- map-demo/app/assets/javascripts/map.js
+++ map-demo/app/assets/javascripts/map.js
@@ -1,11 +1,54 @@
-var mymap = L.map('mapid').setView([54.085173,-4.899902], 5);
+var streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYXNobGVpZ2hobyIsImEiOiJjaXN6d3l6MjUwMDYxMnlvM2djajZudG1zIn0.hAdrsz6R7-4Z9OepbVmMOA',
+                          {id: 'ashleighho.1ccd1a7b',
+                           attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 18,
-  id: 'ashleighho.1ccd1a7b',
-  accessToken: 'pk.eyJ1IjoiYXNobGVpZ2hobyIsImEiOiJjaXN6d3l6MjUwMDYxMnlvM2djajZudG1zIn0.hAdrsz6R7-4Z9OepbVmMOA'
-}).addTo(mymap);
+                            }),
+    light     = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYXNobGVpZ2hobyIsImEiOiJjaXN6d3l6MjUwMDYxMnlvM2djajZudG1zIn0.hAdrsz6R7-4Z9OepbVmMOA',
+                            {id: 'ashleighho.1d82ma2b',
+                              attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+
+                            });
+
+var map = L.map('mapid', {
+  center: [54.085173,-4.899902],
+  zoom: 5,
+  layers: [streets, light]
+});
+
+var baseMaps = {
+  "Light": light,
+  "Streets": streets
+};
+
+var controls = L.control.layers(baseMaps, null, {collapsed:false}).addTo(map);
+
+var htmlObject = controls.getContainer();
+
+var layerControlDiv = document.getElementById('layer-controls');
+
+function setParent(el, newParent)
+{
+  newParent.appendChild(el);
+}
+setParent(htmlObject, layerControlDiv);
+
+
+
+
+
+
+
+
+//
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//   maxZoom: 18,
+//   id: 'ashleighho.1ccd1a7b',
+//   accessToken: 'pk.eyJ1IjoiYXNobGVpZ2hobyIsImEiOiJjaXN6d3l6MjUwMDYxMnlvM2djajZudG1zIn0.hAdrsz6R7-4Z9OepbVmMOA'
+// }).addTo(mymap);
+
+
+
 
 /*var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
