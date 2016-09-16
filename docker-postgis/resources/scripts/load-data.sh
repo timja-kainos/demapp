151eccca21fc6875f83f5736bc8692424764b610
lukem-kainos <l.matthews@kainos.com>
2016-09-16 10:53:50 +0100

    Updated load-data script to pull in ward data. data availible at https://hub.kainos.com/f/9c337? unzip data and put into /demapp/docker-postgis/resources/data

--- docker-postgis/resources/scripts/load-data.sh
+++ docker-postgis/resources/scripts/load-data.sh
@@ -7,8 +7,13 @@ sleep 2
 shp2pgsql -s SRID ../data/Westminster_Parliamentary_Constituencies_December_2015_Full_Extent_Boundaries_in_Great_Britain.shp \
     | psql -d gis > /tmp/import1.log 2>/tmp/import1.err
 
+shp2pgsql -s SRID ../data/Census_Merged_Wards_December_2011_Generalised_Clipped_Boundaries_in_England_and_Wales.shp \
+    | psql -d gis > /tmp/import1.log 2>/tmp/import1.err
+
 psql -d gis -f load.sql
 
 psql -d gis -c "\copy postcode_lookup FROM '../data/National_Statistics_Postcode_Lookup_August_2016_Centroids.csv' DELIMITER ',' CSV"
 
 psql -d gis -c "\copy population FROM '../data/population.csv' DELIMITER ',' CSV"
+
+
